dfe6d40210c71135d924ecd12d138198b86c2ee9
jbainbri <jbainbri@redhat.com>
2020-02-16 10:28:47 +1000

    cache_affinity parse local files

--- cache_affinity.sh
+++ cache_affinity.sh
@@ -5,31 +5,37 @@
 # processor cache. It then reports which processor cores each cache level
 # is shared with.
 #
-# (C) 2014 Red Hat, Inc.
+# (C) 2014-2020 Red Hat, Inc.
 # License: Creative Commons Zero
-#
 
-NRCPUS=$(egrep -c "^processor" /proc/cpuinfo)
+PROCPATH="/proc/cpuinfo"
+SYSPATH="/sys/devices/system/cpu"
+if [[ -a ./proc/cpuinfo && -a ./sys/devices/system/cpu ]]; then
+    PROCPATH="./proc/cpuinfo"
+    SYSPATH="./sys/devices/system/cpu"
+fi
+
+NRCPUS=$(egrep -c "^processor" "$PROCPATH")
 let NRCPUS=$NRCPUS-1
 
 for CPU in $(seq 0 $NRCPUS); do
   echo "Processor $CPU"
-  for LEVEL in $(ls /sys/devices/system/cpu/cpu$CPU/cache/); do
+  for LEVEL in $(ls "$SYSPATH/cpu$CPU/cache/"); do
     for FILE in level type size; do
       echo -n "$LEVEL $FILE is "
-      cat /sys/devices/system/cpu/cpu$CPU/cache/$LEVEL/$FILE
+      cat "$SYSPATH/cpu$CPU/cache/$LEVEL/$FILE"
     done
   done
   echo
 done
 
-INDEXES=$(ls -d /sys/devices/system/cpu/cpu0/cache/index* | wc -l)
+INDEXES=$(eval ls -d "$SYSPATH/cpu0/cache/index*" | wc -l)
 let INDEXES=$INDEXES-1
 
 for CPU in $(seq 0 $NRCPUS); do
   echo "Processor $CPU"
   for INDEX in $(seq 0 $INDEXES); do
     echo -n "CPU $CPU, Index $INDEX = "
-    cat /sys/devices/system/cpu/cpu$CPU/cache/index$INDEX/shared_cpu_map
+    cat "$SYSPATH/cpu$CPU/cache/index$INDEX/shared_cpu_map"
   done
 done
